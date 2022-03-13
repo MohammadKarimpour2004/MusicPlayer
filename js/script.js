df0a50ea13fa31fb1f063e1f27a4b11f1cae08a7
mohammad karimpour <mohammad.karimpour1383@gmail.com>
2022-03-14 01:21:26 +0330

    'player'

--- js/script.js
+++ js/script.js
@@ -1,7 +1,38 @@
-new Vue({
-	el:'#app',
-	data:{
-			musicName:'awat'
-		
-	},
-})
+let audio = document.querySelector('audio')
+let icon = document.querySelector('#icon')
+let img = document.querySelector('img')
+let Status = false 
+
+const playorstop = ()=>{
+	Status = !Status
+
+	if (Status == true) {
+		audio.play()
+		icon.classList.remove('bi-play-fill')
+		icon.classList.add('bi-pause-fill')
+		img.classList.add('animation-img')
+	}else{
+		audio.pause()
+		icon.classList.remove('bi-pause-fill')
+		icon.classList.add('bi-play-fill')
+		img.classList.remove('animation-img')
+	}
+}
+
+let timeMusic =audio.duration /60
+
+const after = ()=>{
+	audio.currentTime = 0
+}
+
+const before = ()=>{
+	audio.currentTime = timeMusic -1
+}
+
+const Front = ()=>{
+	audio.currentTime -= 5
+}
+
+const Back = ()=>{
+	audio.currentTime += 5
+}
