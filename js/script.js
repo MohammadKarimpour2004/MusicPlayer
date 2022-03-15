2aec0a3be06185fa4e6ca45aa735001a67a42b29
mohammad karimpour <mohammad.karimpour1383@gmail.com>
2022-03-15 18:24:16 +0330

    musicplayer

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 let audio = document.querySelector('audio')
 let icon = document.querySelector('#icon')
 let img = document.querySelector('img')
+let list = document.querySelector('.musics')
+let names = document.querySelector('h1')
+let ranges = document.querySelector('input')
 let Status = false 
+let songgIndex = 0
+
+let musics = [
+	{name:'awat_1',music:'./music/music_1.ogg',image:'images/img_1.png'},
+	{name:'awat_2',music:'./music/music_2.ogg',image:'images/img_2.png'},
+	{name:'awat_3',music:'./music/music_3.ogg',image:'images/img_3.png'},
+	{name:'awat_4',music:'./music/music_4.ogg',image:'images/img_4.png'},
+	{name:'awat_5',music:'./music/music_5.ogg',image:'images/img_5.png'}
+]
 
 const playorstop = ()=>{
 	Status = !Status
@@ -19,14 +31,30 @@ const playorstop = ()=>{
 	}
 }
 
+const nextORPrevious = (mm)=>{
+    audio.src = mm.music
+	img.src = mm.image
+	names.innerText = mm.name	
+} 
+
+//musicTime
 let timeMusic =audio.duration /60
 
 const after = ()=>{
-	audio.currentTime = 0
+	songgIndex--;
+  if (songgIndex < 0) {
+    songgIndex = musics.length - 1;
+  }
+  nextORPrevious(musics[songgIndex]);
+  audio.play();
 }
-
 const before = ()=>{
-	audio.currentTime = timeMusic -1
+	songgIndex++;
+  if (songgIndex > musics.length - 1) {
+    songgIndex = 0;
+  }
+  nextORPrevious(musics[songgIndex]);
+  audio.play();
 }
 
 const Front = ()=>{
@@ -36,3 +64,12 @@ const Front = ()=>{
 const Back = ()=>{
 	audio.currentTime += 5
 }
+
+const change = ()=>{
+	audio.currentTime = Number(ranges.value)
+}
+let time = setInterval(()=>{
+	ranges.max = `${audio.duration}`
+	ranges.value =`${audio.currentTime}`
+},1000)
+
